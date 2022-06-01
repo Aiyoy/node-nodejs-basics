c0f088ffdac08646e7562dc53f9686d8fc36d3c8
Aiyoy <ronkaarizona@gmail.com>
2022-06-01 22:20:26 +0300

    feat: add fs/create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,27 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
+
 export const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  fs.access(path.join(__dirname, 'files', 'fresh.txt'), (err) => {
+    if (!err) {
+      console.error('FS operation failed')
+      return
+    }
+  
+    fs.writeFile(
+      path.join(__dirname, 'files', 'fresh.txt'),
+      'I am fresh and young',
+      (err) => {
+        if (err) throw console.log('FS operation failed');
+        console.log('Create')
+      }
+    );
+  })
 };
+
+create();
