c150bc0d70e205897d95124a0fdd93a652ce9fd0
Aiyoy <ronkaarizona@gmail.com>
2022-06-02 15:24:32 +0300

    feat: add solution into cli/args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,11 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  console.log(args);
+  args.forEach((arg, index) => {
+    if (!arg.indexOf('--')) {
+      console.log(`${arg.substr(2)} is ${args[index + 1]}`);
+    }
+  });
 };
+
+parseArgs();
