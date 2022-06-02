ae7cc2fe12a1f861411837a6f971ec2608d10e58
Aiyoy <ronkaarizona@gmail.com>
2022-06-02 17:15:21 +0300

    feat: add streams/read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,15 +1,14 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import fs from 'fs';
-import fsProm from 'fs/promises';
 import path from 'path';
 
 export const read = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
 
-  fs.readFile(path.join(__dirname, 'files', 'fileToRead.txt'), (error, data) => {
-    if (error) return console.error('FS operation failed');
+  fs.readFile(path.join(__dirname, 'file', 'fileToRead.txt'), (error, data) => {
+    if (error) return console.error(new Error('FS operation failed'));
     const text = data.toString();
     console.log(text);
   });
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
+  const { stdout } = process;
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  let readableStream = fs.createReadStream(path.join(__dirname, 'files', 'fileToRead.txt'), "utf8");
+
+  readableStream.on('data', (data) => { 
+    stdout.write(data);
+  });
 };
+
+read();
