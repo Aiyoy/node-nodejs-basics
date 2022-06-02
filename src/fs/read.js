3324ac119868c4b92b6a7907b728c9c1c4084dc9
Aiyoy <ronkaarizona@gmail.com>
2022-06-02 13:39:47 +0300

    fead: add solution into fs/read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import fsProm from 'fs/promises';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  fs.readFile(path.join(__dirname, 'files', 'fileToRead.txt'), (error, data) => {
+    if (error) return console.error('FS operation failed');
+    const text = data.toString();
+    console.log(text);
+  });
 };
+
+read();
