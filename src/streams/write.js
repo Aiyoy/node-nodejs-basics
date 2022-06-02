61020b07d40718b1bacac22289d4a235ba19f5fe
Aiyoy <ronkaarizona@gmail.com>
2022-06-02 17:46:50 +0300

    feat: add streams/write solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
+
 export const write = async () => {
-    // Write your code here 
+  const { stdin } = process;
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const { stdout } = process;
+
+  let writeableStream = fs.createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'));
+
+  stdin.on('data', data => writeableStream.write(data));
 };
+
+write();
