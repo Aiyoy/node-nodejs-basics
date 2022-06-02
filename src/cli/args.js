fe379c813871a26e51d533e4c7e1f4e3e5416c90
Aiyoy <ronkaarizona@gmail.com>
2022-06-02 20:40:41 +0300

    feat: add streams/transform solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,6 +1,5 @@
 export const parseArgs = () => {
   const args = process.argv;
-  console.log(args);
   args.forEach((arg, index) => {
     if (!arg.indexOf('--')) {
       console.log(`${arg.substr(2)} is ${args[index + 1]}`);
