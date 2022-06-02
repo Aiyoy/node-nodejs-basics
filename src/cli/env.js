5da1a065cc8e1bc232947d1a21adc5e78a465cf6
Aiyoy <ronkaarizona@gmail.com>
2022-06-02 15:01:49 +0300

    feat: add solution into cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+  const variables = process.env;
+  const keys = Object.keys(variables).filter((key) => {
+    if (!key.indexOf('RSS_')) return key;
+  });
+  keys.forEach((key) => {
+    console.log(`${key}=${variables[key]}`);
+  });
 };
+
+parseEnv();
