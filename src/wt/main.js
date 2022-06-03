b6225210f38b489aab00eae33db9dcddc9fe3627
Aiyoy <ronkaarizona@gmail.com>
2022-06-03 21:25:32 +0300

    feat: add wt tasks solutions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,30 @@
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import os from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const performCalculations = async () => {
-    // Write your code here
+  const numCPUs = os.cpus().length;
+
+  const workerPromises = [];
+
+  for (let i = 0; i < numCPUs; i++) {
+    workerPromises[i] = new Promise((resolve, reject) => {
+      const worker = new Worker(path.join(__dirname, 'worker.js'), { workerData: 10 + i });
+      worker.on('message', msg => {
+        resolve(msg);
+      });
+      worker.on('error', msg => {
+        reject(msg);
+      });
+    }) 
+  }
+
+  const resArr = await Promise.all(workerPromises);
+  console.log(resArr);
 };
+
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,21 @@
-// n should be received from main thread
+import { workerData, parentPort, BroadcastChannel } from 'worker_threads';
+
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  const res = nthFibonacci(workerData);
+  
+  const inf = {
+    status: 'error',
+    data: null
+  }
+
+  if (res) {
+    inf.status = 'resolved';
+    inf.data = res;
+  }
+
+  parentPort.postMessage(inf);
 };
+
+sendResult();
